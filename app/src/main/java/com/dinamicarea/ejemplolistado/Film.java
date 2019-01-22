c88cc13f2fc8dfbf094148a145e56f6523359d39
Julián Marqués García <jfmargar@gmail.com>
2019-01-22 12:28:13 +0100

    sincronización

---
+++ app/src/main/java/com/dinamicarea/ejemplolistado/Film.java
@@ -0,0 +1,22 @@
+package com.dinamicarea.ejemplolistado;
+
+public class Film {
+    String title;
+    String posterUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+}
